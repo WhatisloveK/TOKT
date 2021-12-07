58891ad3c156fb5b24ddd549fe2767d61546c2f3
Каплюк Владислав Олегович <kuplykvlad@gmail.com>
2021-12-07 03:23:55 +0200

    playing around with P invoke

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,46 @@
+﻿using ConsoleApplication.Models;
+using System;
+using System.Runtime.InteropServices;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        [DllImport("NativeLibrary.dll")]
+        public static extern void HelloWorld();
+
+        [DllImport("NativeLibrary.dll", EntryPoint = "Add")]
+        public static extern int AddNumbers(int num1, int num2);
+
+        [DllImport("NativeLibrary.dll")]
+        public static extern bool IsLengthGreaterThan5(string value);
+
+        [DllImport("NativeLibrary.dll")]
+        [return: MarshalAs(UnmanagedType.BStr)]
+        public static extern string GetName();
+
+        [DllImport("NativeLibrary.dll")]
+        public static extern Shoe CreateShoe(double shoeSize);
+
+        [DllImport("NativeLibrary.dll")]
+        public static extern void BuyShoe(Shoe shoe);
+
+        static void Main(string[] args)
+        {
+            WishList wishList = new WishList("Life");
+
+            wishList.Name = "Birthday";
+
+            wishList.AddItem("Lamp");
+            wishList.AddItem("Graphics Card");
+            wishList.AddItem("Chipotle Burrito Bowl");
+            wishList.RemoveItem("Lamp");
+
+            Console.WriteLine($"{wishList.Name}: {wishList.Count} items");
+
+            wishList.Print();
+
+            Console.ReadLine();
+        }
+    }
+}
